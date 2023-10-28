55e6c8a0f0568c72ed8f35a9995406a7a1a1ce92
Yuvraj <yyuvraj54@gmail.com>
2023-10-28 23:46:38 +0530

    Signup Activity Created

---
+++ app/src/main/java/com/example/nego/Auth/signup.kt
@@ -0,0 +1,13 @@
+package com.example.nego.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nego.R
+
+class signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
+
